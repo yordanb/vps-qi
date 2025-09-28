45a0ff59e62e30e560c54e79f05b4f888eba43e5
yordanb <masyordan@gmail.com>
2025-09-28 14:29:12 +0800

    update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST || '192.168.100.54', // nama service docker
+  host: process.env.DB_HOST || 'posgre13', // nama service docker
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASS || '123456',
+  password: process.env.DB_PASS || 'posgres',
   database: process.env.DB_NAME || 'postgres',
   max: 10,
   idleTimeoutMillis: 30000
@@ -13,7 +13,7 @@ const pool = new Pool({
 // Tes koneksi saat startup
 pool.connect()
   .then(client => {
-    console.log(`✅ Connected to PostgreSQL at ${process.env.DB_HOST || 'localhost'}:${process.env.DB_PORT || 5432}`);
+    console.log(`✅ Connected to PostgreSQL at ${process.env.DB_HOST || 'posgre13'}:${process.env.DB_PORT || 5432}`);
     client.release();
   })
   .catch(err => {
